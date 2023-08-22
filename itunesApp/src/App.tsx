f3f009e630a96bd5002eade48d1ab078da12da5f
Salam <sharqiyem@yandex.ru>
2023-08-22 20:43:51 +0800

    setup absolute path imports

--- itunesApp/src/App.tsx
+++ itunesApp/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Text, View, SafeAreaView} from 'react-native';
+import {ListItem} from 'components';
 
 function App(): JSX.Element {
   return (
     <SafeAreaView className="flex-1">
       <View className="flex-1 justify-center items-center">
         <Text className="text-xl text-blue-500 dark:text-white">Hello</Text>
+        <ListItem />
       </View>
     </SafeAreaView>
   );
